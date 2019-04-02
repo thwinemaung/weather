93a7f1b5ea42f3a1fccc8a8321d3a829c856b662
thwin <thwinemaung@gmail.com>
2019-04-02 18:53:42 +0700

    Add button for missions

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -8,13 +8,12 @@ class ForecastsController < ApplicationController
 
   def more_weather
     @forecast = Forecast.new(:lat => 14.0754804, :lng => 100.6100677)
-    @weather = @forecast.get_weather_data
-    @daily_summary = @weather.daily.summary
-    @daily_weather = @weather.daily.data.first(7)
-    @forecast.summary = @weather.daily.summary
-
     @forecast.save
-    # @hourly_weather = @weather.hourly
+    @weather = @forecast.get_weather_data
+    # @daily_summary = @weather.daily.summary
+    @daily_weather = @weather.daily.data.first(7)
+    # @forecast.summary = @weather.daily.summary
+    @hourly_weather = @weather.hourly.data.first(23)
   end
 
 end
