8c7bf4769c1d21324c6c9ea7f51e392ab91917b4
thwin <thwinemaung@gmail.com>
2019-04-07 20:55:47 +0700

    push to heroku

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -16,4 +16,10 @@ class ForecastsController < ApplicationController
     @hourly_weather = @weather.hourly.data.first(23)
   end
 
+  def delivery
+    @forecast = Forecast.new(:lat => 14.0754804, :lng => 100.6100677)
+    @forecast.save
+    @weather = @forecast.get_weather_data
+    @current_weather = @weather.currently
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'forecasts#show'
+  get 'forecasts/delivery'
   get 'forecasts/more_weather'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
